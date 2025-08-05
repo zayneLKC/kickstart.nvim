41de8a44c6ab55e0475f9b4583a1bc60811f60ac
zayneLKC <zelkaissi@uchicago.edu>
2025-08-05 16:09:37 -0500

    cmp searches buffer; added friendly-snippets for improved cmp

--- init.lua
+++ init.lua
@@ -660,12 +660,12 @@ require('lazy').setup({
           -- `friendly-snippets` contains a variety of premade snippets.
           --    See the README about individual language/framework/plugin snippets:
           --    https://github.com/rafamadriz/friendly-snippets
-          -- {
-          --   'rafamadriz/friendly-snippets',
-          --   config = function()
-          --     require('luasnip.loaders.from_vscode').lazy_load()
-          --   end,
-          -- },
+          {
+            'rafamadriz/friendly-snippets',
+            config = function()
+              require('luasnip.loaders.from_vscode').lazy_load()
+            end,
+          },
         },
       },
       'saadparwaiz1/cmp_luasnip',
@@ -752,6 +752,7 @@ require('lazy').setup({
           { name = 'nvim_lsp' },
           { name = 'luasnip' },
           { name = 'path' },
+          { name = 'buffer' }, -- buffer words
           { name = 'latex_symbols' },
         },
       }
@@ -761,6 +762,7 @@ require('lazy').setup({
           { name = 'nvim_lsp' },
           { name = 'luasnip' },
           { name = 'path' },
+          { name = 'buffer' }, -- buffer words
           -- no latex_symbols here
         },
       })
